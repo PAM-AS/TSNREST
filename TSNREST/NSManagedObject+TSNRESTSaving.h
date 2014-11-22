43f1e3be44cc669006b0b4f7f3074e4bbadb85c6
thomassnielsen <me@thomassnielsen.com>
2014-11-22 13:00:09 +0100

    Create TSNRESTSaving category

---
+++ TSNREST/NSManagedObject+TSNRESTSaving.h
@@ -0,0 +1,13 @@
+//
+//  NSManagedObject+TSNRESTSaving.h
+//  Pods
+//
+//  Created by Thomas Sunde Nielsen on 22.11.14.
+//
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObject (TSNRESTSaving)
+
+@end
---
+++ TSNREST/NSManagedObject+TSNRESTSaving.m
@@ -0,0 +1,13 @@
+//
+//  NSManagedObject+TSNRESTSaving.m
+//  Pods
+//
+//  Created by Thomas Sunde Nielsen on 22.11.14.
+//
+//
+
+#import "NSManagedObject+TSNRESTSaving.h"
+
+@implementation NSManagedObject (TSNRESTSaving)
+
+@end
