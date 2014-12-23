e2e107e3ae4738f432fb65b675daeea63c35251d
thomassnielsen <me@thomassnielsen.com>
2014-12-23 18:01:46 +0100

    More tests

--- Example/ExampleTests/ManagerTests.m
+++ Example/ExampleTests/ManagerTests.m
@@ -23,7 +23,7 @@
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.exampleHeaders = nil;
     [super tearDown];
 }
 
