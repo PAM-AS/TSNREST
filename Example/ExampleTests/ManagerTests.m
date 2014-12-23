ac4940fe515b1fd9f6c006abafb5e5180f98dda2
thomassnielsen <me@thomassnielsen.com>
2014-12-23 12:30:31 +0100

    Add tests for custom headers

---
+++ Example/ExampleTests/ManagerTests.m
@@ -0,0 +1,53 @@
+//
+//  ManagerTests.m
+//  Example
+//
+//  Created by Thomas Sunde Nielsen on 23.12.14.
+//  Copyright (c) 2014 PAM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "TSNRESTManager.h"
+
+@interface ManagerTests : XCTestCase
+
+@property (nonatomic, strong) NSDictionary *exampleHeaders;
+
+@end
+
+@implementation ManagerTests
+
+- (void)setUp {
+    [super setUp];
+    self.exampleHeaders = @{@"X-PAM-Current-Employee":@55, @"X-PAM-Current-User":@234, @"X-PAM-Current-Shop":@92};
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testThatAddingCustomHeadersWorks {
+    TSNRESTManager *manager = [[TSNRESTManager alloc] init];
+    
+    XCTAssert(self.exampleHeaders.count > 0, @"No example headers given, test would succeed when it shouldn't.");
+    
+    for (NSString *key in self.exampleHeaders) {
+        id value = [self.exampleHeaders objectForKey:key];
+        [manager setGlobalHeader:value forKey:key];
+    }
+    
+    for (NSString *key in manager.customHeaders) {
+        
+        XCTAssertEqualObjects([manager.customHeaders objectForKey:key], [self.exampleHeaders objectForKey:key], @"Header values wasn't equal: %@ != %@", [manager.customHeaders objectForKey:key], [self.exampleHeaders objectForKey:key]);
+    }
+}
+
+//- (void)testPerformanceExample {
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
+//}
+
+@end
