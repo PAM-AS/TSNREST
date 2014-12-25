f75de6049e85695d8f647c8096841eaa388d7c80
thomassnielsen <me@thomassnielsen.com>
2014-12-25 12:27:35 +0100

    More tests and test data

--- Example/Example/AppDelegate.m
+++ Example/Example/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "MagicalRecord.h"
+#import "MagicalRecord+Setup.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +18,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [MagicalRecord setupAutoMigratingCoreDataStack];
     return YES;
 }
 
