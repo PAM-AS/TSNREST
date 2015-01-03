a3864ac36127383b917790e52a0c6fcd18b387fb
thomassnielsen <me@thomassnielsen.com>
2015-01-03 10:44:08 +0100

    Attempting a solution to #316.

--- TSNREST/TSNRESTParser.m
+++ TSNREST/TSNRESTParser.m
@@ -33,71 +33,73 @@
 
     __block NSInteger objects = 0;
     
-    [MagicalRecord saveWithBlockAndWait:^(NSManagedObjectContext *localContext) {
-        /*
-         Strategy for not having to delete temporary objects: Always parse objects of type self first, and create the object that way.
-         */
-        
-        NSString *idKey = [(TSNRESTManagerConfiguration *)[[TSNRESTManager sharedManager] configuration] localIdName];
-        if (object && ![object isDeleted] && [object valueForKey:idKey] == nil) {
-            for (NSString *dictKey in dict)
-            {
-                TSNRESTObjectMap *map = [[TSNRESTManager sharedManager] objectMapForServerPath:dictKey];
-                if (map && [map classToMap] == [object class]) {
-                    NSArray *jsonData = [dict objectForKey:dictKey];
-                    if (jsonData.count == 1) {
-                        NSNumber *systemId = [[jsonData objectAtIndex:0] valueForKey:@"id"];
-                        if (systemId) {
-                            NSManagedObject *localObject = [object MR_inContext:localContext];
-                            [localObject setValue:systemId forKey:idKey];
-                            [localContext MR_saveOnlySelfAndWait];
-                            [[object managedObjectContext] refreshObject:object mergeChanges:NO];
+    @synchronized(self) { // Self is the class when using class methods
+        [MagicalRecord saveWithBlockAndWait:^(NSManagedObjectContext *localContext) {
+            /*
+             Strategy for not having to delete temporary objects: Always parse objects of type self first, and create the object that way.
+             */
+            
+            NSString *idKey = [(TSNRESTManagerConfiguration *)[[TSNRESTManager sharedManager] configuration] localIdName];
+            if (object && ![object isDeleted] && [object valueForKey:idKey] == nil) {
+                for (NSString *dictKey in dict)
+                {
+                    TSNRESTObjectMap *map = [[TSNRESTManager sharedManager] objectMapForServerPath:dictKey];
+                    if (map && [map classToMap] == [object class]) {
+                        NSArray *jsonData = [dict objectForKey:dictKey];
+                        if (jsonData.count == 1) {
+                            NSNumber *systemId = [[jsonData objectAtIndex:0] valueForKey:@"id"];
+                            if (systemId) {
+                                NSManagedObject *localObject = [object MR_inContext:localContext];
+                                [localObject setValue:systemId forKey:idKey];
+                                [localContext MR_saveOnlySelfAndWait];
+                                [[object managedObjectContext] refreshObject:object mergeChanges:NO];
 #if DEBUG
-                            NSLog(@"Set id %@ to newly created object of class %@", [object valueForKey:idKey], NSStringFromClass([object class]));
+                                NSLog(@"Set id %@ to newly created object of class %@", [object valueForKey:idKey], NSStringFromClass([object class]));
+#endif
+                            }
+#if DEBUG
+                            else {
+                                NSLog(@"Returned object didn't have a valid ID, skipping updating the object.");
+                            }
 #endif
                         }
 #if DEBUG
                         else {
-                            NSLog(@"Returned object didn't have a valid ID, skipping updating the object.");
+                            NSLog(@"Got more than one object of the type we saved Can't know which, skipping updating saved object.");
                         }
 #endif
                     }
-#if DEBUG
-                    else {
-                        NSLog(@"Got more than one object of the type we saved Can't know which, skipping updating saved object.");
-                    }
-#endif
                 }
             }
-        }
 #if DEBUG
-        else {
-            NSLog(@"No valid object to update, proceeding to parse as usual.");
-        }
-#endif
-        
-        
-        for (NSString *dictKey in dict)
-        {
-            TSNRESTObjectMap *map = [[TSNRESTManager sharedManager] objectMapForServerPath:dictKey];
-            if (map)
-            {
-                NSArray *jsonData = [dict objectForKey:dictKey];
-                objects += jsonData.count;
-                [jsonData deserializeWithMap:map inContext:localContext optimize:objects > 100];
-            }
-#if DEBUG
-            else
-            {
-                NSLog(@"No object map found for %@. Bailing out.", dictKey);
+            else {
+                NSLog(@"No valid object to update, proceeding to parse as usual.");
             }
 #endif
-        }
-        
+            
+            
+            for (NSString *dictKey in dict)
+            {
+                TSNRESTObjectMap *map = [[TSNRESTManager sharedManager] objectMapForServerPath:dictKey];
+                if (map)
+                {
+                    NSArray *jsonData = [dict objectForKey:dictKey];
+                    objects += jsonData.count;
+                    [jsonData deserializeWithMap:map inContext:localContext optimize:objects > 100];
+                }
 #if DEBUG
-        NSLog(@"Parsing %lu arrays (%li objects) took %f", (unsigned long)dict.count, (long)objects, [NSDate timeIntervalSinceReferenceDate] - start);
+                else
+                {
+                    NSLog(@"No object map found for %@. Bailing out.", dictKey);
+                }
 #endif
-    }];
+            }
+            
+#if DEBUG
+            NSLog(@"Parsing %lu arrays (%li objects) took %f", (unsigned long)dict.count, (long)objects, [NSDate timeIntervalSinceReferenceDate] - start);
+#endif
+        }];
+    }
     [self doneWithCompletion:completion dict:dict];
 }
 
