69a843265b3442b307c2eff8ff24db41aac297c8
thomassnielsen <me@thomassnielsen.com>
2015-01-14 15:10:49 +0100

    Clean up TSNRESTDataTask cat header

--- TSNREST/NSURLSessionDataTask+TSNRESTDataTask.h
+++ TSNREST/NSURLSessionDataTask+TSNRESTDataTask.h
@@ -10,7 +10,14 @@
 
 @interface NSURLSessionDataTask (TSNRESTDataTask)
 
-+ (NSURLSessionDataTask *)dataTaskWithRequest:(NSURLRequest *)request success:(void (^)(NSData *data, NSURLResponse *response, NSError *error))successBlock failure:(void (^)(NSData *data, NSURLResponse *response, NSError *error, NSInteger statusCode))failureBlock finally:(void (^)(NSData *data, NSURLResponse *response, NSError *error))finallyBlock;
-+ (NSURLSessionDataTask *)dataTaskWithRequest:(NSURLRequest *)request success:(void (^)(NSData *data, NSURLResponse *response, NSError *error))successBlock failure:(void (^)(NSData *data, NSURLResponse *response, NSError *error, NSInteger statusCode))failureBlock finally:(void (^)(NSData *data, NSURLResponse *response, NSError *error))finallyBlock parseResult:(BOOL)parseResult;
++ (NSURLSessionDataTask *)dataTaskWithRequest:(NSURLRequest *)request
+                                      success:(void (^)(NSData *data, NSURLResponse *response, NSError *error))successBlock
+                                      failure:(void (^)(NSData *data, NSURLResponse *response, NSError *error, NSInteger statusCode))failureBlock
+                                      finally:(void (^)(NSData *data, NSURLResponse *response, NSError *error))finallyBlock;
+
++ (NSURLSessionDataTask *)dataTaskWithRequest:(NSURLRequest *)request
+                                      success:(void (^)(NSData *data, NSURLResponse *response, NSError *error))successBlock
+                                      failure:(void (^)(NSData *data, NSURLResponse *response, NSError *error, NSInteger statusCode))failureBlock
+                                      finally:(void (^)(NSData *data, NSURLResponse *response, NSError *error))finallyBlock parseResult:(BOOL)parseResult;
 
 @end
