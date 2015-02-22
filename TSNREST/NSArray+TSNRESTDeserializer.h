ca9bc366223446924f411b683298337549b2f59a
thomassnielsen <me@thomassnielsen.com>
2015-02-22 19:47:35 +0100

    Add documentation to the NSArray+TSNRESTDeserializer header

--- TSNREST/NSArray+TSNRESTDeserializer.h
+++ TSNREST/NSArray+TSNRESTDeserializer.h
@@ -11,6 +11,13 @@
 
 @interface NSArray (TSNRESTDeserializer)
 
+/**
+ Converts all contained JSON objects to NSManagedObjects by using the specified object map, and saves them to the store.
+ 
+ @param map The object map to use
+ @param localContext The NSManagedObjectContext to use
+ @param optimize Set whether to skip objects where the optimizableKey (set in TSNRESTManagerConfiguration) hasn't changed
+ */
 - (void)deserializeWithMap:(TSNRESTObjectMap *)map inContext:(NSManagedObjectContext *)localContext optimize:(BOOL)optimize;
 
 @end
